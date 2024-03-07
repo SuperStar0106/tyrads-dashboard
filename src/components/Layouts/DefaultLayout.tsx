d25a944d6c1999b8f084a8122f99c635dfd77a72
OskarBrown <oskar.brownz@gmail.com>
2024-03-08 01:49:52 +0200

    Hide the scrollbar of context sector

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -13,7 +13,13 @@ export default function DefaultLayout({
     <>
       <div className="flex h-screen overflow-hidden">
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        <div
+          className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden"
+          style={{
+            scrollbarWidth: "none",
+            msOverflowStyle: "none",
+          }}
+        >
           <main>
             <div className="mx-auto max-w-screen-2xl bg-slate-200 h-screen p-4 md:p-6 2xl:p-10">
               {children}
